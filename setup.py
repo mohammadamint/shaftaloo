788890a0645b504e24ddb53c4d6a2fe2b32df62b
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2023-03-01 11:29:27 +0100

    update

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import find_packages,setup
 setup(
     name = "shaftaloo",
     description= "An open-source python package",
+    long_description="asd"
     url= "https://github.com/mohammadamint/shaftaloo/",
-    version = "0.0.1",
+    version = "0.0.2",
     packages= find_packages(),
     license="European Union Public License 1.2",
     python_requires = ">=3.6.0",
